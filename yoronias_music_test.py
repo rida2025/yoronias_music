67861decff5712cad486dd2e569607149553e910
rida2015 <90518506+rida2025@users.noreply.github.com>
2023-03-13 08:36:43 +0100

    working stage (:
    everything allright

--- yoronias_music_test.py
+++ yoronias_music_test.py
@@ -21,9 +21,6 @@ class Bot(commands.Bot):
     async def setup_hook(self):
         await self.add_cog(Music(bot))
 
-
-
-
 class Music(commands.Cog):
     """Music cog to hold Wavelink related commands and listeners."""
 
@@ -34,8 +31,10 @@ class Music(commands.Cog):
     async def connect_nodes(self):
         """Connect to our Lavalink nodes."""
         await self.bot.wait_until_ready()
-        await wavelink.NodePool.create_node(bot=self.bot, host='lava1.horizxon.studio', port=80,
-                                            password="horizxon.studio")
+        await wavelink.NodePool.create_node(bot=self.bot,
+                                            host='lavalink2.devamop.in',
+                                            port=8830,
+                                            password="DevamOP")
 
 
     @commands.Cog.listener()
@@ -46,23 +45,46 @@ class Music(commands.Cog):
 
     @app_commands.command(name="play", description="this is the working play")
     async def play(self, interaction: discord.Interaction, *, search: str):
-        """Play a song with the given search query.
 
-        If not connected, connect to our voice channel.
-        """
-
-        search = await wavelink.YouTubeTrack.search(search)
+        search_results = await wavelink.YouTubeTrack.search(search)
+        search = search_results[0]
 
         if not interaction.guild.voice_client:
             vc: wavelink.Player = await interaction.user.voice.channel.connect(cls=wavelink.Player)
         else:
             vc: wavelink.Player = interaction.guild.voice_client
 
-        track = search[0]
+        if vc.queue.is_empty and not vc.is_playing():
+            await vc.queue.put_wait(search)
+            vc.autoplay = True
+            await vc.play(await vc.queue.get_wait())
+            await interaction.response.send_message(f"Now playing `{search.title}` by `{search.author}`",
 
-        await vc.play(track)
-        await interaction.response.send_message(f"Now playing `{track.title}` by `{track.author}`",
                                                 allowed_mentions=discord.AllowedMentions.none())
+        else:
+            await vc.queue.put_wait(search)
+            await interaction.response.send_message(f'Added `{search.title}` to the queue...')
+
+    @app_commands.command(name="queue", description="Show the current queue")
+    async def queue(self, interaction: commands.Context):
+        vc: wavelink.Player = interaction.guild.voice_client
+
+        if not vc or vc.queue.is_empty:
+            await interaction.response.send_message("The queue is currently empty.", ephemeral=True)
+        else:
+            tracks = [f"{i + 1}. {track.title}" for i, track in enumerate(vc.queue)]
+            queue_message = "\n".join(tracks)
+            await interaction.response.send_message(f"Current Queue:\n{queue_message}", ephemeral=True)
+
+    @app_commands.command(name="skip", description="to skip the current music")
+    async def skip(self, interaction: commands.Context):
+        vc: wavelink.Player = interaction.guild.voice_client
+        if not vc.queue.is_empty:
+            await vc.stop()
+            search = await vc.queue.get_wait()
+            await vc.play(search)
+            # {wavelink.Player.queue.history[vc.queue.history.find_position(wavelink.Player.current) + 1]}
+            await interaction.response.send_message(f"Now playing `{search.title}` by `{search.author}", delete_after=5)
 
     @app_commands.command(name="pause", description="to pause the music")
     async def pause(self, interaction):
@@ -71,7 +93,7 @@ class Music(commands.Cog):
             await voice.pause()
             await interaction.response.send_message('pause...')
         else:
-            await interaction.response.send_message(ephemeral='the bot is not in a voice chat')
+            await interaction.response.send_message('the bot is not in a voice chat')
 
     @app_commands.command(name="leave", description="to leave the voice chat")
     async def leave(self, interaction):
@@ -80,7 +102,7 @@ class Music(commands.Cog):
             await voice.disconnect()
             await interaction.response.send_message('stop...')
         else:
-            await interaction.response.send_message(ephemeral='the bot is not in a voice chat')
+            await interaction.response.send_message('the bot is not in a voice chat')
 
     @app_commands.command(name="resume", description="to resume the music after pause")
     async def resume(self, interaction):
@@ -94,16 +116,40 @@ class Music(commands.Cog):
             # Resume playback if paused
         if voice.is_paused():
             await voice.resume()
-            await interaction.response.send_message(ephemeral='Resuming playback.')
+            await interaction.response.send_message('Resuming playback.')
             return
 
     @app_commands.command(name="clear", description="to clear lines")
-    async def purge(self, interaction, amount: int):
+    async def clear(self, ctx: Context, amount: int):
         amount = amount + 1
-        await interaction.response.defer()
-        await interaction.channel.purge(limit=amount)
-        await interaction.followup.send(ephemeral="Messages have been cleared")
+        await ctx.response.defer()
+        await ctx.channel.purge(limit=amount)
+        await ctx.followup.send(f"```{amount-1} Messages will be cleared```")
 
+    @app_commands.command(name="help", description="a help command")
+    async def help(self, interaction: discord.Interaction):
+        await interaction.response.send_message('```play,leave,clear,resume,pause,skip```')
+
+    @app_commands.command(name="volume", description="from 0 to 100")
+    async def volume(self, interaction: commands.Context, value: int):
+        value = value * 10
+        vc: wavelink.Player = interaction.guild.voice_client
+
+        if vc and vc.is_playing():
+            await vc.set_volume(value)
+            value = value / 10
+            await interaction.response.send_message(f"``` the volume is {value}```")
+        else:
+            await interaction.response.send_message('the bot is not in a voice chat')
+
+    @app_commands.command(name="stop", description="to stop the music some time it skip xD")
+    async def stop(self, interaction):
+        voice = interaction.guild.voice_client
+        if voice and voice.is_playing():
+            await voice.stop()
+            await interaction.response.send_message('stop...')
+        else:
+            await interaction.response.send_message('the bot is not in a voice chat')
 
 
 
@@ -152,4 +198,4 @@ async def sync(
             ret += 1
 
     await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
-bot.run("ODc4MjY4OTQ1MDc1OTM3Mjkw.GgOI-_.9f_wo78QUu265dtF1qRoAS-VP60fAKItq3qqOU")
+bot.run("TOKEN")
